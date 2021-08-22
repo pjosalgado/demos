0890c1e4a0133a11e25b05c609f3222ff9acd271
Paulo Salgado <pjosalgado@gmail.com>
2021-08-21 23:33:13 -0300

    Kafka multi groupId test with Java.

---
+++ kafka-multi-groupid-java/demo2/src/main/java/com/example/demo2/EventListener.java
@@ -0,0 +1,16 @@
+package com.example.demo2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class EventListener {
+
+    @KafkaListener(topics = "${topics.groupid-test}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumer(String order) {
+        log.info("Message received: " + order);
+    }
+
+}
